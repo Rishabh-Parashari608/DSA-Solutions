35f39bb4b8982eb085dd7337b6e2692854a385f6
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-10 14:42:49 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/1510. Stone Game IV/Solution.java
+++ LeetCode/Java/Hard/1510. Stone Game IV/Solution.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        int root = (int)Math.sqrt(n);
-        if (root * root == n) {
-            return true;
-        } else if ({
-            else {
-            return false;
-        }
+        
     }
 }
