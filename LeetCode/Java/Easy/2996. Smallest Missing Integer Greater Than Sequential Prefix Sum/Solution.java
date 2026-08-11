644a116a3ea2c45c2d2599e2c2723c14683ed337
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-11 10:52:16 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
+++ LeetCode/Java/Easy/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
@@ -4,10 +4,10 @@ class Solution {
         ArrayList<Integer> prefix = new ArrayList<>();
         prefix.add(nums[0]);
         prefix.add(nums[1]);
-        int d = nums[1] - nums[0];
+        
         int sum = nums[0] + nums[1];
         for (int i = 2; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1] + d) {
+            if (nums[i] == nums[i - 1] + 1) {
                 prefix.add(nums[i]);
                 sum += nums[i];
             }
