d9bdbbe4dfe963d3709eff2346804783435638c5
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-11 10:10:27 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/Solution.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
 class Solution {
     public int missingInteger(int[] nums) {
-        
+        ArrayList<Integer> prefix = new ArrayList<>();
+        prefix.add(nums[0]);
+        prefix.add(nums[1]);
+        int d = nums[1] - nums[0];
+        int sum = nums[0] + nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + d) {
+                prefix.add(nums[i]);
+                sum += nums[i];
+            }
+        }
         Arrays.sort(nums);
-        return (nums[nums.length-1] + 1);
+        for (int num : nums) {
+            if (num == sum) {
+                sum++;
+            }
+        }
+        return sum;
     }
 }
