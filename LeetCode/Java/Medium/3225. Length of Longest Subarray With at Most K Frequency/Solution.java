ec58a14cbedba269211d38d88468eb4454cb7a0d
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-12 10:53:28 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/3225. Length of Longest Subarray With at Most K Frequency/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int n = set.size();
+
+        return n * k;
+    }
+}
