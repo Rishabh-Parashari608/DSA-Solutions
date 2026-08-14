b295284b72d0df89ba283c9747d10925a318f24b
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-14 23:36:26 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/500. Keyboard Row/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public String[] findWords(String[] words) {
+        List<String> result = new ArrayList<>();
+
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        for (String word : words) {
+            String lower = word.toLowerCase();
+
+            String row;
+            
+            if (row1.indexOf(lower.charAt(0)) != -1) {
+                row = row1;
+            } else if (row2.indexOf(lower.charAt(0)) != -1) {
+                row = row2;
+            } else {
+                row = row3;
+            }
+
+            boolean valid = true;
+
+            for (char ch : lower.toCharArray()) {
+                if (row.indexOf(ch) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
