5ccf3cf3fdbc9b652aaf26ed293e1b64f8d1448a
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-16 17:12:37 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/697. Degree of an Array/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        int[] count = new int[50000];
+        int[] first = new int[50000];
+
+        Arrays.fill(first, -1);
+
+        int degree = 0;
+        int minLength= nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (first[num] == -1) {
+                first[num] = i;
+            }
+            count[num]++;
+
+            if (count[num] > degree) {
+                degree = count[num];
+                minLength = i - first[num] + 1;
+            }
+        }
+        return minLength;
+    }
+}
