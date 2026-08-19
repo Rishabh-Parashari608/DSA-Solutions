c85dfab378b2d1fece77128de1d319d0d5664019
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-19 09:39:04 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/1496. Lucky Numbers in a Matrix/Solution.java
+++ LeetCode/Patterns/Arrays/1496. Lucky Numbers in a Matrix/Solution.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[] rowMin = new int[m];
-        int[] colMax = new int[n];
-
-        Arrays.fill(rowMin, Integer.MAX_VALUE);
-        Arrays.fill(colMax, Integer.MIN_VALUE);
-
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rowMin[i] = Math.min(rowMin[i], matrix[i][j]);
-                colMax[j] = Math.max(colMax[j], matrix[i][j]);
-            }
-        }
+            int minCol = 0;
 
-        List<Integer> res = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) {
-                    res.add(matrix[i][j]);
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] < matrix[i][minCol]) {
+                    minCol = j;
                 }
+            }
+
+            int value = matrix[i][minCol];
+
+            boolean isMax = true;
+            for (int j = 0; j < m; j++) {
+                if (matrix[j][minCol] > value) {
+                    isMax = false;
+                    break;
+                }
+            }
+
+            if (isMax) {
+                res.add(value);
             }
         }
 
