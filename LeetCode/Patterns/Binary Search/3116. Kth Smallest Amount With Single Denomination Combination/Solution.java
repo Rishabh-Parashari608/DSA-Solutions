952e655ce99c15c95d1a0c3a456d238a66bcfc28
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-21 14:26:34 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/3116. Kth Smallest Amount With Single Denomination Combination/Solution.java
+++ LeetCode/Patterns/Binary Search/3116. Kth Smallest Amount With Single Denomination Combination/Solution.java
@@ -1,17 +1,74 @@
 import java.util.*;
 
 class Solution {
-    public long findKthSmallest(int[] coins, int k) {
-        List<Integer> value = new ArrayList<>();
 
-        for (int i = 0; i < coins.length; i++) {
-            for (int n = 1; n < 25; n++) {
-                value.add(coins[i] * n);
+    public long findKthSmallest(int[] coins, int k) {
+        int n = coins.length;
+
+        long low = 1;
+        long high = (long) coins[0] * k;
+
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+
+            long count = countNumbers(mid, coins);
+
+            if (count >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
 
-        Collections.sort(value);
+        return low;
+    }
 
-        return value.get(k);
+    private long countNumbers(long x, int[] coins) {
+        int n = coins.length;
+        long count = 0;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long lcm = 1;
+            int bits = 0;
+            boolean valid = true;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bits++;
+
+                    long gcd = gcd(lcm, coins[i]);
+                    lcm = lcm / gcd * coins[i];
+
+                    if (lcm > x) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) {
+                continue;
+            }
+
+            long multiples = x / lcm;
+
+            if (bits % 2 == 1) {
+                count += multiples;
+            } else {
+                count -= multiples;
+            }
+        }
+
+        return count;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
     }
 }
