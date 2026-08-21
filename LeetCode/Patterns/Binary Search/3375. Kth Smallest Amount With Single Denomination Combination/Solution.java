bcfeaa776225b4fa2bf2f87d7b5e1db16f3d1761
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-21 13:44:28 +0530

    Add Java Solution to Patterns/Binary Search

---
+++ LeetCode/Patterns/Binary Search/3375. Kth Smallest Amount With Single Denomination Combination/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        List<Integer> value = new ArrayList<>();
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int n = 1; n < 25; n++) {
+                value.add(coins[i] * n);
+            }
+        }
+
+        int n = value.size();
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            res.add(value.get(i));
+        }
+
+        Collections.sort(res);
+
+        return res.get(k);
+    }
+}
