0ea5bd8293eaaf49a736398c617cd610f4c79343
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-08-21 16:01:50 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1386. Shift 2D Grid/Solution.java
+++ LeetCode/Java/Easy/1386. Shift 2D Grid/Solution.java
@@ -2,22 +2,14 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int total = m * n;
+        int total = m*n;
         k %= total;
-        int[][] shifted = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int curr = i * n + j;
-                int next = (curr + k) % total;
-
-                shifted[next / n][next % n] = grid[i][j]; 
-            }
-        }
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                row.add(shifted[i][j]);
+                int pos = (i * n + j - k + total) % total;
+                row.add(grid[pos / n][pos % n]);
             }
             ans.add(row);
         }
