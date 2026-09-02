fd540f654791c28f196f0479014a8ff6e5c64c41
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-09-02 13:35:55 +0530

    Add Easy Java Solution for 824. Goat Latin from LeetCode

---
+++ LeetCode/Patterns/Strings/824. Goat Latin/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] words = sentence.split(" ");
+        for(int i = 0; i < words.length; i++) {
+            char c = words[i].charAt(0);
+            if(c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                words[i] = words[i].substring(1) + c;
+            }
+            
+        }
+        for(int j = 0; j < words.length; j++) {
+            words[j] = words[j] + "m";
+        }
+        
+        String ext = "aa";
+        int k = 0;
+        while(k < words.length) {
+            words[k] = words[k] + ext;
+            ext += "a";
+            k++;
+        }
+        String result = String.join(" ", words);
+        return result;
+    }
+}
