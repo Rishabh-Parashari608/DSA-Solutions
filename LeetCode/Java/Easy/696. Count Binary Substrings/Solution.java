7a534b680a7098fb67511269341139cb8677bed1
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-09-05 18:39:34 +0530

    Add Easy Java Solution for 696. Count Binary Substrings from LeetCode

--- LeetCode/Java/Easy/696. Count Binary Substrings/Solution.java
+++ LeetCode/Java/Easy/696. Count Binary Substrings/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-        char[] arr = s.toCharArray();
-        int countZero = 0;
-        int countOne = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '0') {
-                countZero += 1;
+        int prev = 0;
+        int curr = 1;
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                curr++;
             } else {
-                countOne += 1;
+                count += Math.min(prev, curr);
+                prev = curr;
+                curr = 1;
             }
-        } 
-        int count = Math.min(countZero, countOne);
-        return count + 2;
+        }
+        count += Math.min(prev, curr);
+
+        return count;
     }
 }
--- LeetCode/Patterns/Two Pointers/696. Count Binary Substrings/README.md
+++ LeetCode/Patterns/Two Pointers/696. Count Binary Substrings/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 11 ms
+- **Memory:** 46.6 MB
 
 ---
 
