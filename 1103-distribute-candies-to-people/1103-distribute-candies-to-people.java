70db1e013cdec7725246d8b6f933ec4d6b825f41
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-09-17 22:55:08 +0530

    Time: 3 ms (6.81%), Space: 42.7 MB (19.31%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int i = 0;
+        int give = 1;
+
+        while (candies > 0) {
+            ans[i % num_people] += Math.min(candies, give);
+            candies -= Math.min(candies, give);
+            give++;
+            i++;
+        }
+
+        return ans;
+    }
+}
