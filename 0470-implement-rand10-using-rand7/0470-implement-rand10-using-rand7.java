5f0a9074a9622522913ae2c5844ce74715577e94
Rishabh-Parashari608 <parasharirishabh608@gmail.com>
2026-09-26 17:28:41 +0530

    Time: 5 ms (97.14%), Space: 51.5 MB (92%) - LeetHub

---
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
@@ -0,0 +1,12 @@
+
+class Solution extends SolBase {
+    public int rand10() {
+        while(true) {
+            int num = (rand7() - 1) * 7 + rand7();
+
+            if (num <= 40) {
+                return (num - 1) % 10 + 1;
+            }
+        }
+    }
+}
